077d4679a3781623ad11f5b25bd4856bfeddc876
bmuday <bmuday@live.fr>
2022-07-12 15:15:18 +0000

    init search project

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,10 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+<template><div>hello</div></template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+};
 </script>
 
 <style>
